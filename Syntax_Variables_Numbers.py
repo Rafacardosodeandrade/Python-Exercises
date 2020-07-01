af159931149b0270efb2ee6905a6ca671c5006f8
Rafael <rafacardosodeandrade@gmail.com>
2020-07-01 11:19:34 -0500

    radius and area

--- Syntax_Variables_Numbers.py
+++ Syntax_Variables_Numbers.py
@@ -1,1 +1,13 @@
-Testa
+print("You've successfully run some Python code")
+print("Congratulations!")
+print("Let's go ahead and never stop")
+
+pi = 3.14159 # approximate
+diameter = 3
+
+# Create a variable called 'radius' equal to half the diameter
+radius = diameter / 2
+____
+q1.hint()
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+area = pi * radius ** 2

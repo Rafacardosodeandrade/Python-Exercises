758919ed3ccc09c0db4903d271a6352240a346d0
Rafael <rafacardosodeandrade@gmail.com>
2020-07-08 16:55:24 -0500

    count_item

--- Conditional.py
+++ Conditional.py
@@ -327,7 +327,49 @@ def best_items(racers):
     return winner_item_counts
 
 
-# 
+# He tried it on a small example list above and it seemed to work correctly:
+
+sample = [
+    {'name': 'Peach', 'items': ['green shell', 'banana', 'green shell',], 'finish': 3},
+    {'name': 'Bowser', 'items': ['green shell',], 'finish': 1},
+    {'name': None, 'items': ['mushroom',], 'finish': 2},
+    {'name': 'Toad', 'items': ['green shell', 'mushroom'], 'finish': 1},
+]
+best_items(sample)
+
+#OUTPUT
+# WARNING: Encountered racer with unknown name on iteration 3/4 (racer = None)
+# {'green shell': 2, 'mushroom': 1}
+
+# However, when he tried running it on his full dataset, the program crashed with a TypeError.
+
+# Can you guess why? Try running the code cell below to see the error message Luigi is getting.
+# Once you've identified the bug, fix it in the cell below (so that it runs without any errors).
+
+# Hint: Luigi's bug is similar to one we encountered in the tutorial when we talked about star imports.
+
+def best_items(racers):
+    winner_item_counts = {}
+    for i in range(len(racers)):
+        # The i'th racer dictionary
+        racer = racers[i]
+        # We're only interested in racers who finished in first
+        if racer['finish'] == 1:
+            for item in racer['items']:
+                # Add one to the count for this item (adding it to the dict if necessary)
+                if i not in winner_item_counts:
+                    winner_item_counts[i] = 0
+                winner_item_counts[i] += 1
+
+        # Data quality issues :/ Print a warning about racers with no name set. We'll take care of it later.
+        if racer['name'] is None:
+            print("WARNING: Encountered racer with unknown name on iteration {}/{} (racer = {})".format(
+                i+1, len(racers), racer['name'])
+                 )
+    return winner_item_counts
+
+# Try analyzing the imported full dataset
+best_items(full_dataset)
 
 
 

b0d789fec8287db1cb3eab1de36fd7cb1ef6fbef
Rafael <rafacardosodeandrade@gmail.com>
2020-07-08 16:47:29 -0500

    Count_items_in_dictionaries

--- Conditional.py
+++ Conditional.py
@@ -286,6 +286,52 @@ def prettify_graph(graph):
     # Set the new labels
     graph.set_yticklabels(new_labels)
     
+# 2. 🌶️🌶️
+# This is a very hard problem. Feel free to skip it if you are short on time:
+
+# Luigi is trying to perform an analysis to determine the best items for winning races 
+# on the Mario Kart circuit. He has some data in the form of lists of 
+# dictionaries that look like...
+
+[
+    {'name': 'Peach', 'items': ['green shell', 'banana', 'green shell',], 'finish': 3},
+    {'name': 'Bowser', 'items': ['green shell',], 'finish': 1},
+    # Sometimes the racer's name wasn't recorded
+    {'name': None, 'items': ['mushroom',], 'finish': 2},
+    {'name': 'Toad', 'items': ['green shell', 'mushroom'], 'finish': 1},
+]
+'items' is a list of all the power-up items the racer picked up in that race, and 'finish' 
+was their placement in the race (1 for first place, 3 for third, etc.).
+
+# He wrote the function below to take a list like this and return a dictionary mapping 
+# each item to how many times it was picked up by first-place finishers.
+
+def best_items(racers):
+    """GIven a list of racer dictionaries, return a dictionary mapping items to the number 
+    of times those items were picked up by racers who finished in first place.
+    """
+    winner_item_counts = {}
+    for i in range(len(racers)):
+        # The i'th racer dictionary
+        racer = racers[i]
+        # We're only interessed in racers who finished in first
+        if racer['finish'] == 1:
+            for i in racer['items']:
+                # Add one to the count for this item (adding it to the dict if necessary)
+                if i not in winner_item_counts:
+                    winner_item_counts[i] = 0
+                winner_item_counts[i] +=1
+        # Data quality issues :/ Print a warning about racers with no name set. We'll take care of it later.
+        if racer['name'] is None:
+            print("WARNING: Encoutered racer with unknown name on interation {}/{} (racer = {})".format(i+1, len(racers), racer['name']))
+    return winner_item_counts
+
+
+# 
+
+
+
+
        
 
 

3b85d4d7f088bdf2570230b9a9af775482ff42e2
Rafael <rafacardosodeandrade@gmail.com>
2020-07-07 20:31:54 -0500

    new_labels

--- Conditional.py
+++ Conditional.py
@@ -278,6 +278,11 @@ def prettify_graph(graph):
     graph.set_ylim(bottom=0) 
     # Label the yaxis
     graph.set_ylabel("Balance")
+    # Bonus: format the numbers on the y-axis as dollar amounts
+    # An array of the values displayed on the y-axis (150, 175, 200, etc.)
+    ticks = graph.get_yticks()
+    # Format those values into strings beginning with dollar sign
+    new_labels = ['${}'.format(int(amt)) for amt in ticks]
        
 
 

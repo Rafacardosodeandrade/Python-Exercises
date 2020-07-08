6b9e247d8a4a23099529cdeff48f5ef4d1790b3d
Rafael <rafacardosodeandrade@gmail.com>
2020-07-07 20:23:52 -0500

    graph.set_title

--- Conditional.py
+++ Conditional.py
@@ -256,3 +256,26 @@ from learntools.python import jimmy_slots
 # Call the get_graph() function to get Jmmy's graph
 graph = jimmy_slots.get_graph()
 graph
+
+# As you can see, he's hit a bit of bad luck recently. He wants to tweet this along with some choice emojis, but, as it looks right now, his followers will probably find it confusing. He's asked if you can help him make the following changes:
+
+# Add the title "Results of 500 slot machine pulls"
+# Make the y-axis start at 0.
+# Add the label "Balance" to the y-axis
+# After calling type(graph) you see that Jimmy's graph is of type matplotlib.axes._subplots.AxesSubplot. Hm, that's a new one. By calling dir(graph), you find three methods that seem like they'll be useful: .set_title(), .set_ylim(), and .set_ylabel().
+
+# Use these methods to complete the function prettify_graph according to Jimmy's requests. We've already checked off the first request for you (setting a title).
+
+# (Remember: if you don't know what these methods do, use the help() function!)
+
+def prettify_graph(graph):
+    """Modify the given graph according to Jimmy's requests: add a title, make the y-axis
+    start at 0, label the y-axis. (And, if you're feeling ambitious, format the tick marks
+    as dollar amounts using the "$" symbol.)
+    """
+    graph.set_title("Result of 500 slot machine pulls")
+        
+
+
+
+

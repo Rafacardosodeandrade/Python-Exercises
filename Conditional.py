d55dbca865ac8ab92e5ec53e675a3f9bef3d0752
Rafael <rafacardosodeandrade@gmail.com>
2020-07-09 17:33:53 -0500

    function_balckjack_ higher_hand

--- Conditional.py
+++ Conditional.py
@@ -431,7 +431,22 @@ def hand_total(hand):
             total += int(card)
     # At this point, total is the sum of this hand's cards *not counting aces*
 
-    
+    # Add aces, counting them as 1 for now. This is the smallest total we can make from this hand
+    total += aces
+    # "Upgrade" aces from 1 to 11 as long as it helps us get closer to 21
+    # without busting
+    while total + 10 <= 21 and aces > 0:
+        # Upgrade an ace from 1 to 11
+        total += 10
+        aces -= 1
+    return total
+
+def blackjack_hand_greater_than(hand_1, hand_2):
+    total_1 = hand_total(hand_1)
+    total_2 = hand_total(hand_2)
+    return total_1 <= 21 and (total_1 > total_2 or total_2 > 21)
+
+
 
 
        

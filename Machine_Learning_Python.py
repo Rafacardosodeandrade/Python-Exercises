be5398bdae059b4d2c4950b7e8d4356df29f1ae3
Rafael <rafacardosodeandrade@gmail.com>
2020-07-10 13:52:10 -0500

    print_df=>df.describe()

--- Machine_Learning_Python.py
+++ Machine_Learning_Python.py
@@ -7,3 +7,9 @@ iowa_file_path = '.../input/home-data-for-ml-course/train.csv'
 # Fill in the line below to read the file into a variable home_data
 home_data = pd.read_csv(iowa_file_path)
 
+# Step 2: Review The Data
+# Use the command you learned to view summary statistics of the data.
+# Then fill in variables to answer the following questions
+
+# Print summary statistics in next line
+home_data.describe()

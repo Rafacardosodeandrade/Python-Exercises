0c9c4fa6eab5d90ae1e67ebbdbe5573f76c945e2
Rafael <rafacardosodeandrade@gmail.com>
2020-07-12 18:24:54 -0500

    bring_data

---
+++ Selecting_Data_for_Modeling.py
@@ -0,0 +1,16 @@
+# This is a exemplo made by Kaggle.com - it's very important to show step by step how Select Data for Modeling.
+# 
+# SELECTING DATA FOR MODELING
+# Your dataset had too many variables to wrap your head around, or even to print out nicely. 
+# How can you pare down this overwhelming amount of data to something you can understand?
+# To choose variables/columns, we'll need to see a list of all columns in the dataset.
+# That is done with the columns property of the DataFrame (the bottom line of code below).
+
+#step 1: import PANDAS (specific library to manipulate Dataframes) and bring dataframe (csv file).
+#the name of DATAFRAME is melbourne
+import pandas as pd 
+
+melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.csv' #bring data
+
+
+

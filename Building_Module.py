ff59c6ca1ac7c36581c673f75166f0efc9af1db9
Rafael <rafacardosodeandrade@gmail.com>
2020-07-13 19:34:55 -0500

    choose_model_decisiontree_decision_tree

---
+++ Building_Module.py
@@ -0,0 +1,19 @@
+# Building Your Model
+#
+# You will use the scikit-learn library to create your models. 
+# When coding, this library is written as sklearn, as you will see in the sample code.
+# Scikit-learn is easily the most popular library for modeling the types of data 
+# typically stored in DataFrames.
+#
+# 1. Define: What type of model will it be? A decision tree? 
+# Some other type of model? Some other parameters of the model type are specified too.
+# 2. Fit: Capture patterns from provided data. This is the heart of modeling.
+# 3. Predict: Just what it sounds like
+# 4. Evaluate: Determine how accurate the model's predictions are.
+#
+from sklearn.tree import DecisionTreeRegressor
+
+# Define model.
+# Specify a number for random_state to ensure same results each run
+
+

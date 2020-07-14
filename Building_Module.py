5bcf53de1576f8f6006daa207a3254212f163e55
Rafael <rafacardosodeandrade@gmail.com>
2020-07-13 19:53:31 -0500

    make_predict_model_run

--- Building_Module.py
+++ Building_Module.py
@@ -26,8 +26,33 @@ DecisionTreeRegressor(criterion='mse', max_depth=None, max_features=None,
                       min_samples_split=2, min_weight_fraction_leaf=0.0,
                       presort=False, random_state=1, splitter='best')
 
+# Many machine learning models allow some randomness in model training. 
+# Specifying a number for random_state ensures you get the same results in each run. 
+# This is considered a good practice. You use any number, and model quality won't 
+# depend meaningfully on exactly what value you choose.
+#
+# We now have a fitted model that we can use to make predictions.
+# 
+# In practice, you'll want to make predictions for new houses coming on the market 
+# rather than the houses we already have prices for. But we'll make predictions for
+# the first few rows of the training data to see how the predict function works.
 
 
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()))
+
+#OUTPUT
+Making predictions for the following 5 houses:
+   Rooms  Bathroom  Landsize  Lattitude  Longtitude
+1      2       1.0     156.0   -37.8079    144.9934
+2      3       2.0     134.0   -37.8093    144.9944
+4      4       1.0     120.0   -37.8072    144.9941
+6      3       2.0     245.0   -37.8024    144.9993
+7      2       1.0     256.0   -37.8060    144.9954
+The predictions are
+[1035000. 1465000. 1600000. 1876000. 1636000.]
 
 
 

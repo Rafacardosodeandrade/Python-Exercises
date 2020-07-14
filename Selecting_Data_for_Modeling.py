7eb074a1d6187341c5bd2b6e8eca983318161258
Rafael <rafacardosodeandrade@gmail.com>
2020-07-13 19:05:43 -0500

    head

--- Selecting_Data_for_Modeling.py
+++ Selecting_Data_for_Modeling.py
@@ -59,7 +59,17 @@ X = melbourne_data[melbourne_features]
 # Let's quickly review the data we'll be using to predict house prices using the describe 
 # method and the head method, which shows the top few rows.
 
-X.describe()
+X.describe() # OUTPUT is a table in Jupyter Notebook
 
+	Rooms	            Bathroom	      Landsize	      Lattitude	      Longtitude
+count	6196.000000	      6196.000000 	6196.000000 	6196.000000	      6196.000000
+mean	2.931407	      1.576340	      471.006940       	-37.807904	      144.990201
+std	0.971079    	0.711362	      897.449881  	0.075850	      0.099165
+min	1.000000    	1.000000	      0.000000	      -38.164920	      144.542370
+25%	2.000000	      1.000000	      152.000000	      -37.855438	      144.926198
+50%	3.000000	      1.000000	      373.000000  	-37.802250	      144.995800
+75%	4.000000	      2.000000	      628.000000	      -37.758200	      145.052700
+max	8.000000	      8.000000	      37000.000000	-37.457090	      145.526350
 
+X.head() # OUTPUT is a table in Jupyter Notebook
 

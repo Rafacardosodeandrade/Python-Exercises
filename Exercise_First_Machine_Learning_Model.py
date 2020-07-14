395664fd3368b784fd4237d5551fcdec2aca9015
Rafael <rafacardosodeandrade@gmail.com>
2020-07-14 16:07:47 -0500

    Creating_X_or_features

--- Exercise_First_Machine_Learning_Model.py
+++ Exercise_First_Machine_Learning_Model.py
@@ -50,3 +50,28 @@ print(home_data)
 [1460 rows x 81 columns]
 
 y = home_data['SalePrice']
+
+# Step 2: Create X
+# Now you will create a DataFrame called X holding the predictive features.
+#
+# Since you want only some columns from the original data, you'll first
+# create a list with the names of the columns you want in X.
+#
+# You'll use just the following columns in the list (you can copy and paste
+# the whole list to save some typing, though you'll still need to add quotes):
+#
+# * LotArea
+# * YearBuilt
+# * 1stFlrSF
+# * 2ndFlrSF
+# * FullBath
+# * BedroomAbvGr
+# * TotRmsAbvGrd
+#
+# After you've created that list of features, use it to create the DataFrame
+# that you'll use to fit the model.
+#
+# Create the list of features below 
+feature_names = ['LotArea','YearBuilt','1stFlrSF', '2ndFlrSF', 'FullBath','BedroomAbvGr','TotRmsAbcGrd']
+# Select data corresponding to features in feature_names
+X = home_data[feature_names]

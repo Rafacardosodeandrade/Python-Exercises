565e1b13b3a9175d3a165116480d3c20b64819db
Rafael <rafacardosodeandrade@gmail.com>
2020-07-15 18:04:16 -0500

    mean_error

--- Exercise_First_Machine_Learning_Model.py
+++ Exercise_First_Machine_Learning_Model.py
@@ -134,6 +134,27 @@ melbourne_data = pd.read_csv(melbourne_file_path)
 # Filter rows with missing price values
 filtered_melbourne_data = melbourne_data.dropna(axis=0)
 
+# Choose target and features
+y = filtered_melbourne_data.Price
+melbourne_features = ['Rooms','Bathroom','Landsize','BuildingArea', 'YearBuilt'.'Lattitude', 'Longitude']
+
+X = filtered_melbourne_data[melbourne_features]
+
+from sklearn.tree import DecisionTreeRegressor
+# Define model
+melbourne_model = DecisionTreeRegressor()
+# Fit model
+melbourne_model.fit(X, y)
+
+# Once we have a model, here is how we calculate the mean absolute error:
+
+from sklearn.metrics import mean_ansolute_error
+
+predicted_home_prices = melbourne_model.predict(X)
+mean_absolute_error(y, predicted_home_prices)
+
+# OUTPUT
+434.71594577146544
 
 
 

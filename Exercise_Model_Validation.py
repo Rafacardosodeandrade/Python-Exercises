fd5b2f3aa096c4d245588ce6be2f243ebc4469c4
Rafael <rafacardosodeandrade@gmail.com>
2020-07-16 17:23:45 -0500

    split_data

--- Exercise_Model_Validation.py
+++ Exercise_Model_Validation.py
@@ -19,3 +19,23 @@ iowa_model.fit(X, y)
 print("First in-sample predictions:", iowa_model.predict(X.head()))
 print("Actual target values for those homes:", y.head().tolist())
 
+#OUTPUT
+First in-sample predictions: [208500. 181500. 223500. 140000. 250000.]
+Actual target values for those homes: [208500, 181500, 223500, 140000, 250000]
+
+##########################################################################################
+# #########################################################################################
+#   STEP 1
+#
+#   Split Your Data
+#   Use the train_test_split function to split up your data.
+#   Give it the argument random_state=1 so the check functions know what to expect when verifying your code.
+#   Recall, your features are loaded in the DataFrame X and your target is loaded in y.
+#
+from sklearn.model_selection import train_test_split # Import
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+
+
+
+

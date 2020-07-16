a4aa67fdc7f9b01e072e9178065e16826b4b543e
Rafael <rafacardosodeandrade@gmail.com>
2020-07-16 17:35:59 -0500

    Specify_the_model_DecisionTree

--- Exercise_Model_Validation.py
+++ Exercise_Model_Validation.py
@@ -26,14 +26,37 @@ Actual target values for those homes: [208500, 181500, 223500, 140000, 250000]
 ##########################################################################################
 # #########################################################################################
 #   STEP 1
-#
 #   Split Your Data
+#
 #   Use the train_test_split function to split up your data.
 #   Give it the argument random_state=1 so the check functions know what to expect when verifying your code.
 #   Recall, your features are loaded in the DataFrame X and your target is loaded in y.
 #
 from sklearn.model_selection import train_test_split # Import
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+#
+#
+#   STEP 2
+#   Create a `DecisionTreeRegressor` model and fit it to the relevant data.
+#   Set `random_state` to 1 again when creating the model.
+#
+# IMPORTANT: You imported DecisionTreeRegressor in your last exercise
+# and that code has been copied to the setup code above. So, no need to
+# import it again
+#
+#   Specify the model
+iowa_model = DecisionTreeRegressor(random_state=1)
+#   Fit the model with training data
+iowa_model.fit(train_X, train_y)
+#
+# OUTPUT
+[186500. 184000. 130000.  92000. 164500. 220000. 335000. 144152. 215000.
+ 262000.]
+[186500. 184000. 130000.  92000. 164500. 220000. 335000. 144152. 215000.
+ 262000.]
+
+
+
 
 
 

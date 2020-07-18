eefa1873b404d192320462a7fc79074ef5d0311d
Rafael <rafacardosodeandrade@gmail.com>
2020-07-18 14:57:20 -0500

    Rename_columns

--- Pandas_libray_exercise.py
+++ Pandas_libray_exercise.py
@@ -1,3 +1,13 @@
 import pandas as pd 
 df = pd.read_csv ('winequality-red.csv', sep=';')
 df.head()
+
+#   Renaming Column
+new_df = df.rename(columns)={'fixed acidity': 'fixed_acidity',
+                             'volatile acidity': 'volatile_acidity',
+                             'citric acid': 'citric_acid',
+                             'residual sugar': 'residual_sugar',
+                             'free sulfur dioxide': 'free_sulfur_dioxide',
+                             'total sulfur dioxide': 'total_sulfur_dioxide'
+                            })
+new_df.head()

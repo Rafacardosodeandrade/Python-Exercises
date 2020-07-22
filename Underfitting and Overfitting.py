0df3e32f63a4dda9d3d47993c367de28d66d2c10
Rafael <rafacardosodeandrade@gmail.com>
2020-07-21 20:16:48 -0500

    underfitting and overfitting

--- Underfitting and Overfitting.py
+++ Underfitting and Overfitting.py
@@ -1,3 +1,30 @@
 # Code you have used to load the data 
 import pandas as pd 
 from sklearn.metrics import mean_absolute_error
+from sklear.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+# Path of this file
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+# Create target object and call it y
+y = home_data.SalesPrice
+# Create X
+features = ['LotArea','YearBuilt', '1stFlrSF', '2ndFlorSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[features]
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Specify model
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+# Fit model
+iowa_model.fit(train_X, train_y)
+
+# Make Validation predictions and calculate mean absolute error
+val_predictions = iowa_model_predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE: {:,.0f}".format(val_mae))
+

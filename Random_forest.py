b8f8848d65ef3205d15d786d39314e26be53ba92
Rafael <rafacardosodeandrade@gmail.com>
2020-07-24 11:10:00 -0500

    import_RandomForestRegressor_from_sklearn.ensemble

---
+++ Random_forest.py
@@ -0,0 +1,6 @@
+# We build a random forest model similarly to how we built a decision tree in 
+# scikit-learn - this time using the RandomForestRegressor class instead of DecisionTreeRegressor.
+
+from sklearn.ensemble import RandomForestRegressor
+
+

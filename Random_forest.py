eca523271eaf1f516b1104e0bd823c0d924fd317
Rafael <rafacardosodeandrade@gmail.com>
2020-07-24 11:20:39 -0500

    done_example_RandomForestRegressor

--- Random_forest.py
+++ Random_forest.py
@@ -4,3 +4,15 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
+
+# OUTPUT
+/opt/conda/lib/python3.6/site-packages/sklearn/ensemble/forest.py:245: FutureWarning: The default value of n_estimators will change from 10 in version 0.20 to 100 in 0.22.
+  "10 in version 0.20 to 100 in 0.22.", FutureWarning)
+202888.18157951365
+
+
+

0ebe72547e83629db70c66c8e2ad29c33243edf3
Rafael <rafacardosodeandrade@gmail.com>
2020-07-28 18:16:08 -0500

    rd_model_on_full_data

--- Build_Random_Forest_Model.py
+++ Build_Random_Forest_Model.py
@@ -4,4 +4,28 @@
 rf_model_on_full_data = RandomForestRegressor(random_state=1)
 
 # Fit rf_model_on_full_data on all data from the
-rf_model_on_full_data.fit(train_X, train_y)
+rf_model_on_full_data.fit(X, y)
+
+#Make Predictions
+Read the file of "test" data. And apply your model to make predictions
+
+# path to file you will use for predictions
+test_data_path = '../input/test.csv'
+
+# read test data file using pandas
+test_data = pd.read_csv(test_data_path)
+
+# create test_X which comes from test_data but includes only the columns you used for prediction.
+# The list of columns is stored in a variable called features
+test_X = test_data[features]
+
+# make predictions which we will submit.
+test_preds = rf_model_on_full_data.predict(test_X)
+
+# The lines below shows you how to save your data in the format needed to score it in the competition
+
+
+
+
+
+

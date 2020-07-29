3232451fa13b52fec2e8e36dfeedefd244bd3c05
Rafael <rafacardosodeandrade@gmail.com>
2020-07-29 16:57:28 -0500

    Create_Data_Frame_with_index

--- Pandas_Micro_Course/Creating_Read_Writing.py
+++ Pandas_Micro_Course/Creating_Read_Writing.py
@@ -4,7 +4,16 @@
 # |30       |21     |
 # 
 import pandas as pd
-pd.set_option('max_rows', 5)
+#pd.set_option('max_rows', 5)
 
-fruits = pd.DataFrame([[30, 21]], columns=['Apples' , 'Bananas'])
-print(fruits)
+#fruits = pd.DataFrame([[30, 21]], columns=['Apples' , 'Bananas'])
+#print(fruits)
+
+#2. Create a dataframe fruit_sales that matches the diagram below:
+#               |Apples   |Bananas|
+# 2017 Sales    |30       |21     |
+# 2018 Sales    |41       |34     |
+
+fruit_sales = pd.DataFrame([[35, 21],[41, 34]], columns=['Apples','Bananas'], index=['2017 Sales', '2018 Sales'])
+print(fruit_sales)
+

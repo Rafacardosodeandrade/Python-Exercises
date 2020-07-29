0bb596855c0aae1cab1765ea47a475d7cbc5a6e0
Rafael <rafacardosodeandrade@gmail.com>
2020-07-29 16:31:29 -0500

    create_data_Frame_with_Pandas

---
+++ Pandas_Micro_Course/Creating_Read_Writing.py
@@ -0,0 +1,9 @@
+# 1. In the cell below, create a DataFrame fruits that looks like this:
+
+# |Apples   |Bananas|
+# |30       |21     |
+# 
+import pandas as pd
+pd.set_option('max_rows', 5)
+
+fruits = pd.DataFrame([[30, 21]], columns=['Apples' , 'Bananas'])

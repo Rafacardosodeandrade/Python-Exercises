55b9874b60e219c1ef7e332cb25d940292f3292e
Rafael <rafacardosodeandrade@gmail.com>
2020-07-30 18:58:55 -0500

    variable_series

--- Pandas_Micro_Course/Creating_Read_Writing.py
+++ Pandas_Micro_Course/Creating_Read_Writing.py
@@ -4,10 +4,10 @@
 # |30       |21     |
 # 
 import pandas as pd
-#pd.set_option('max_rows', 5)
+pd.set_option('max_rows', 5)
 
-#fruits = pd.DataFrame([[30, 21]], columns=['Apples' , 'Bananas'])
-#print(fruits)
+fruits = pd.DataFrame([[30, 21]], columns=['Apples' , 'Bananas'])
+print(fruits)
 
 #2. Create a dataframe fruit_sales that matches the diagram below:
 #               |Apples   |Bananas|
@@ -16,4 +16,18 @@ import pandas as pd
 
 fruit_sales = pd.DataFrame([[35, 21],[41, 34]], columns=['Apples','Bananas'], index=['2017 Sales', '2018 Sales'])
 print(fruit_sales)
+
+# Create a variable ingredients with a Series that looks like:
+#
+# Flour     4 cups
+# Milk       1 cup
+# Eggs     2 large
+# Spam       1 can
+# Name: Dinner, dtype: object
+
+quantities = ['4 cups', '1 cup','2 large', '1 can']
+items = ['Flour', 'Milk', 'Eggs', 'Spam']
+recipe = pd.Series(quantities, index=items, name='Dinner')
+ingredients = recipe
+print(ingredients)
 

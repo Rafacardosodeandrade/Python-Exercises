83eb5f09e5ba0f2314f3103968d89e783e597518
Rafael <rafacardosodeandrade@gmail.com>
2020-08-03 16:31:31 -0500

    reviews_unique

--- Pandas_Micro_Course/3_Summary_Functions_and_Maps.py
+++ Pandas_Micro_Course/3_Summary_Functions_and_Maps.py
@@ -1,2 +1,5 @@
 # 1 What is the median of the points column in the reviews DataFrame?
 median_points = reviews.points.median()
+
+# 2 What countries are represented in the dataset? (Your answer should not include any duplicates.)
+countries = reviews.countries.unique()

1a59f56eab24ba6a1021a6501463429c0b003a9f
Rafael <rafacardosodeandrade@gmail.com>
2020-08-03 16:34:15 -0500

    import pandas

--- Pandas_Micro_Course/3_Summary_Functions_and_Maps.py
+++ Pandas_Micro_Course/3_Summary_Functions_and_Maps.py
@@ -1,3 +1,7 @@
+import pandas as pd 
+pd.set_option("display_max_rows", 5)
+reviews = pd.read_csv("../input/wine-reviews/winemag-data-130k-v2.csv", index_col=0)
+
 # 1 What is the median of the points column in the reviews DataFrame?
 median_points = reviews.points.median()
 

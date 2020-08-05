0acf05d61710abe9c21db14b8e42c339b70eec44
Rafael <rafacardosodeandrade@gmail.com>
2020-08-04 21:00:21 -0500

    reviews_country

--- Pandas_Micro_Course/3_Summary_Functions_and_Maps.py
+++ Pandas_Micro_Course/3_Summary_Functions_and_Maps.py
@@ -7,10 +7,16 @@ from learntools.pandas.summary_functions_and_maps import *
 
 # 1 What is the median of the points column in the reviews DataFrame?
 median_points = reviews.points.median()
+print(median_points)
 
 # 2 What countries are represented in the dataset? (Your answer should not include any duplicates.)
 countries = reviews.countries.unique()
+print(countries)
 
 # 3 How often does each country appear in the dataset? 
 # Create a Series reviews_per_country mapping countries to the count of reviews of wines from that country.
 reviews_per_country = reviews.country.value_count()
+print(reviews_per_country)
+
+# 4 Create variable centered_price containing a version of the price column with the mean price subtracted.
+centered_price = reviews.price

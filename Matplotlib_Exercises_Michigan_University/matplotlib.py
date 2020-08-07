ea4a3b2a79590d3cebe27de641856473d10441a9
Rafael <rafacardosodeandrade@gmail.com>
2020-08-07 11:57:25 -0500

    import_backend_figure_canvas

--- Matplotlib_Exercises_Michigan_University/matplotlib.py
+++ Matplotlib_Exercises_Michigan_University/matplotlib.py
@@ -3,3 +3,6 @@ mpl.get_backend()
 
 #pyplot is scripting layer - simplifies access to the artist and backend layers
 
+#backend use
+from matplotlib.backends,backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure

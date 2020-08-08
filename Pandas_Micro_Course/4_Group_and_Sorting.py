3f127b95fa037e2adec0d1cfcee2a5e8685e340c
Rafael <rafacardosodeandrade@gmail.com>
2020-08-08 18:57:08 -0500

    2

--- Pandas_Micro_Course/4_Group_and_Sorting.py
+++ Pandas_Micro_Course/4_Group_and_Sorting.py
@@ -4,6 +4,11 @@
 
 reviews_written = reviews.groupby('taster_twitter_handle').taster_twitter_handle.count()
 
+# 2. What is the best wine I can buy for a given amount of money? Create a Series whose 
+# index is wine prices and whose values is the maximum number of points a wine costing 
+# that much was given in a review. Sort the values by price, ascending (so that 4.0 dollars 
+# is at the top and 3300.0 dollars is at the bottom).
+
 
 
 

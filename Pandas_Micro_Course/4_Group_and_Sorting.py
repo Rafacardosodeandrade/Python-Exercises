254164ab3b83c1f61c4bcde2bffcb21b23b6dd02
Rafael <rafacardosodeandrade@gmail.com>
2020-08-10 17:49:03 -0500

    min_and_max_price

--- Pandas_Micro_Course/4_Group_and_Sorting.py
+++ Pandas_Micro_Course/4_Group_and_Sorting.py
@@ -11,6 +11,18 @@ reviews_written = reviews.groupby('taster_twitter_handle').taster_twitter_handle
 
 best_rating_per_price = reviews.groupby('price')['points'].max().sort_index()
 
+# 3. What are the minimum and maximum prices for each variety of wine? Create a DataFrame 
+# whose index is the variety category from the dataset and whose values are the min and max values thereof.
+
+price_extremes = reviews.groupby('variety').price.agg([min, max])
+
+
+
+
+
+
+
+
 
 
 

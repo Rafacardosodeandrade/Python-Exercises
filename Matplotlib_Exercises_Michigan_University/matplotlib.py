740f3cbf4f577139b88e52068f4964e0dd9e123b
Rafael <rafacardosodeandrade@gmail.com>
2020-08-11 10:09:31 -0500

    remove_frame_charts

--- Matplotlib_Exercises_Michigan_University/matplotlib.py
+++ Matplotlib_Exercises_Michigan_University/matplotlib.py
@@ -47,5 +47,27 @@ plt.title('Top 5 Languages for Math & Data \nby % popularity on Stack Overflow',
 
 plt.show()
 
+########
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.figure()
+
+languages =['Python', 'SQL', 'Java', 'C++', 'JavaScript']
+pos = np.arange(len(languages))
+popularity = [56, 39, 34, 34, 29]
+
+plt.bar(pos, popularity, align='center')
+plt.xticks(pos, languages)
+plt.ylabel('% Popularity')
+plt.title('Top 5 Languages for Math & Data \nby % popularity on Stack Overflow', alpha=0.8)
+
+# remove all the ticks (both axes), and tick labels on the Y axis
+plt.tick_params(top='off', bottom='off', left='off', right='off', labelleft='off', labelbottom='on')
+
+# TODO: remove the frame of the chart
+
+plt.show()
+
 
 

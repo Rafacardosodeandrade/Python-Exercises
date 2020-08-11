d08420b309e8f75f7e5ec8e7609175d5786fd898
Rafael <rafacardosodeandrade@gmail.com>
2020-08-11 10:07:42 -0500

    print_bar_chart

--- Matplotlib_Exercises_Michigan_University/matplotlib.py
+++ Matplotlib_Exercises_Michigan_University/matplotlib.py
@@ -28,5 +28,24 @@ plt.plot(1.5,1.5,'o')
 plt.plot(2,2,'o')
 plt.plot(2.5,2.5.'o')
 
+######
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.figure()
+
+languages =['Python', 'SQL', 'Java', 'C++', 'JavaScript']
+pos = np.arange(len(languages))
+popularity = [56, 39, 34, 34, 29]
+
+plt.bar(pos, popularity, align='center')
+plt.xticks(pos, languages)
+plt.ylabel('% Popularity')
+plt.title('Top 5 Languages for Math & Data \nby % popularity on Stack Overflow', alpha=0.8)
+
+#TODO: remove all the ticks (both axes), and tick labels on the Y axis
+
+plt.show()
+
 
 

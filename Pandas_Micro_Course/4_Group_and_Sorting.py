2f981a6b5412e2838d31b1cf73cca53b1eaf57fb
Rafael <rafacardosodeandrade@gmail.com>
2020-08-17 14:16:04 -0500

    size_sort

--- Pandas_Micro_Course/4_Group_and_Sorting.py
+++ Pandas_Micro_Course/4_Group_and_Sorting.py
@@ -27,6 +27,11 @@ sorted_varieties = price_extremes.sort_values(by=['min', 'max'], ascending=False
 
 reviewer_mean_ratings = reviews.groupby('taster_name').points.mean()
 
+# 6. What combination of countries and varieties are most common? Create a Series whose index 
+# is a MultiIndexof {country, variety} pairs. For example, a pinot noir produced in the US should 
+# map to {"US", "Pinot Noir"}. Sort the values in the Series in descending order based on wine count.
+
+country_variety_counts = reviews.groupby(['country', 'variety']).size().sort_values(ascending=False)
 
 
 

742b0c827b021343077ad2f2c0e90972f03cc2b2
Rafael <rafacardosodeandrade@gmail.com>
2020-08-19 12:54:48 -0500

    Series

--- Pandas_Micro_Course/5_Data_and_Missing_Values.py
+++ Pandas_Micro_Course/5_Data_and_Missing_Values.py
@@ -1,2 +1,5 @@
 # 1. What is the data type of the points column in the dataset?
 dtype = reviews.point.dtype 
+
+# 2. Create a Series from entries in the points column, but convert the entries to strings. Hint: strings are str in native Python.
+point_strings = reviews.points.astype(str)

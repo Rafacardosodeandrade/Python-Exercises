1b243797ff9c8ee707516345c051c60cc84197e6
Rafael <rafacardosodeandrade@gmail.com>
2020-08-20 16:03:47 -0500

    question_1

---
+++ ML_Python_Model_coursera/Assignment_1.py
@@ -0,0 +1,23 @@
+# Question 1
+# Scikit-learn works with lists, numpy arrays, scipy-sparse matrices, and pandas DataFrames, so converting the dataset to a DataFrame is not necessary for training this model. Using a DataFrame does however help make many things easier such as munging data, so let's practice creating a classifier with a pandas DataFrame.
+
+# Convert the sklearn.dataset cancer to a DataFrame.
+
+# This function should return a (569, 31) DataFrame with
+
+# columns =
+
+['mean radius', 'mean texture', 'mean perimeter', 'mean area',
+'mean smoothness', 'mean compactness', 'mean concavity',
+'mean concave points', 'mean symmetry', 'mean fractal dimension',
+'radius error', 'texture error', 'perimeter error', 'area error',
+'smoothness error', 'compactness error', 'concavity error',
+'concave points error', 'symmetry error', 'fractal dimension error',
+'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+'worst smoothness', 'worst compactness', 'worst concavity',
+'worst concave points', 'worst symmetry', 'worst fractal dimension',
+'target']
+# and index =
+
+# RangeIndex(start=0, stop=569, step=1)
+

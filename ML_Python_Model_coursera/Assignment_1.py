9b641b578884ddab8109beaccb661b0b0816f3bb
Rafael <rafacardosodeandrade@gmail.com>
2020-08-20 16:27:35 -0500

    q2

--- ML_Python_Model_coursera/Assignment_1.py
+++ ML_Python_Model_coursera/Assignment_1.py
@@ -26,3 +26,13 @@ def answer_one():
     df['target']=cancer.target
     return df
 
+
+# Question 2
+# What is the class distribution? (i.e. how many instances of malignant (encoded 0) and how many benign (encoded 1)?)
+
+# This function should return a Series named target of length 2 with integer values and index = ['malignant', 'benign']
+
+def answer_two():
+    cancerdf = answer_one()
+    Distribution=
+

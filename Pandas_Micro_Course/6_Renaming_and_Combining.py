965e184fe1676caf416ad3a117dbca6ea2f42a9d
Rafael <rafacardosodeandrade@gmail.com>
2020-08-24 15:08:25 -0500

    renaming

---
+++ Pandas_Micro_Course/6_Renaming_and_Combining.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+reviews = pd.read_csv("../input/wine-reviews/winemag-data-130k-v2.csv", index_col=0)
+
+from learntools.core import binder; binder.bind(globals())
+from learntools.pandas.renaming_and_combining import *
+print("Setup complete.")

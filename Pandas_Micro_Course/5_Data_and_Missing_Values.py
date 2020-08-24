176159c55d2a15c85e280d8ed639756915e9ffe9
Rafael <rafacardosodeandrade@gmail.com>
2020-08-23 20:56:55 -0500

    4

--- Pandas_Micro_Course/5_Data_and_Missing_Values.py
+++ Pandas_Micro_Course/5_Data_and_Missing_Values.py
@@ -7,3 +7,8 @@ point_strings = reviews.points.astype(str)
 # 3. Sometimes the price column is null. How many reviews in the dataset are missing a price?
 missing_price_reviews = reviews[reviews.price.isnull()]
 n_missing_prices = len(missing_price_reviews)
+
+# 4. What are the most common wine-producing regions? Create a Series counting the number of 
+# times each value occurs in the region_1 field. This field is often missing data, so replace 
+# missing values with Unknown. Sort in descending order. Your output should look something like this:
+reviews_per_region = reviews.region_1.fillna('Unknown').value_counts().sort_values(ascending=False)

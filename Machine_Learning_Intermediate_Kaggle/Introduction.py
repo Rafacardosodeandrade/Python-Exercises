228534d37d81b8a54f57e8ea34f66c783732a197
Rafael <rafacardosodeandrade@gmail.com>
2020-08-27 18:42:30 -0500

    start

--- Machine_Learning_Intermediate_Kaggle/Introduction.py
+++ Machine_Learning_Intermediate_Kaggle/Introduction.py
@@ -2,5 +2,11 @@
 # Users to predict home prices in Iowa using 79 explanatory variables describing 
 # (almost) every aspect of the homes.
 
-#
+# Run the next code cell without changes to load the training and validation features 
+# in X_train and X_valid, along with the prediction targets in y_train and y_valid. 
+# The test features are loaded in X_test. (If you need to review features and prediction targets,
+#  please check out this short tutorial. To read about model validation, look here. 
+# Alternatively, if you'd prefer to look through a full course to review all of these topics, start here.)
+
+
 

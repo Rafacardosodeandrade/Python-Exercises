5ca21b5e86fba13c77d7bb66c58bf8564889b727
Rafael <rafacardosodeandrade@gmail.com>
2020-08-27 18:45:44 -0500

    break_in_validation_models

--- Machine_Learning_Intermediate_Kaggle/Introduction.py
+++ Machine_Learning_Intermediate_Kaggle/Introduction.py
@@ -21,4 +21,8 @@ features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'Bedroom
 X = X_full[features].copy()
 X_test = X_test_full[features].copy()
 
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                      random_state=0)
+
 

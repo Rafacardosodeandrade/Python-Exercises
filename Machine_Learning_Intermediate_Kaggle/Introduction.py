779606a72e2a9ac8a39a4c32f0dee2d97a12ba6b
Rafael <rafacardosodeandrade@gmail.com>
2020-08-27 18:41:43 -0500

    Machine_Learning_Intermediate

---
+++ Machine_Learning_Intermediate_Kaggle/Introduction.py
@@ -0,0 +1,6 @@
+# You will work with data from the Housing Prices Competition for Kaggle Learn 
+# Users to predict home prices in Iowa using 79 explanatory variables describing 
+# (almost) every aspect of the homes.
+
+#
+

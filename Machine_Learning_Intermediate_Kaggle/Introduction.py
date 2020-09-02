ceed156018df43937d86f1777d148bcffc47fee7
Rafael <rafacardosodeandrade@gmail.com>
2020-09-02 12:53:43 -0500

    function to comparing different models

--- Machine_Learning_Intermediate_Kaggle/Introduction.py
+++ Machine_Learning_Intermediate_Kaggle/Introduction.py
@@ -53,6 +53,10 @@ models = [model_1, model_2, model_3, model_4, model_5]
 from sklearn.metrics import mean_absolute_error
 
 #Function for comparing different models
+def score_model(model, X_t=X_train, X_v=X_valid, y_t=y_train, y_v=y_valid):
+    model.fit(X_t, y_t)
+    preds = model.predict(X_v)
+    return mean_absolute_error(y_v, preds)
 
 
 

5e7b401937f229ce4187d4bd6530f647c9cb4512
Rafael <rafacardosodeandrade@gmail.com>
2020-09-08 19:01:31 -0500

    sum

--- Machine_Learning_Intermediate_Kaggle/Introduction.py
+++ Machine_Learning_Intermediate_Kaggle/Introduction.py
@@ -130,7 +130,7 @@ num_cols_with_missing = 3
 
 # Fill in the line below: How many missing entries are contained in 
 # all of the training data?
-tot_missing = ____
+tot_missing = 212 + 6 + 58
 
 
 

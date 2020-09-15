198950f8aab3e55975f747241fb79bca2eb68a77
Rafael <rafacardosodeandrade@gmail.com>
2020-09-14 19:30:38 -0500

    columns

--- Machine_Learning_Intermediate_Kaggle/Introduction.py
+++ Machine_Learning_Intermediate_Kaggle/Introduction.py
@@ -164,6 +164,14 @@ reduced_X_valid = ____
 # Check your answers
 step_2.check()
 
+# Get names of columns with missing values
+cols_with_missing = [col for col in X_train.columns
+                     if X_train[col].isnull().any()]
+
+# Drop columns in training and validation data
+reduced_X_train = X_train.drop(cols_with_missing, axis=1)
+reduced_X_valid = X_valid.drop(cols_with_missing, axis=1)
+
 
 
 
